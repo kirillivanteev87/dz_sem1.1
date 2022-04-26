dde338634cb2ffa5a10d99203e3976e5d04ff2b0
Ivanteev_Kirill <ivanteev87@mail.ru>
2022-04-26 22:38:42 +0300

    Создали программу Задача8

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает 
+//все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+int numberA, i;
+numberA =8;
+i = 2;
+
+while (i <= numberA)
+ {
+     Console.WriteLine($"{i}, ");
+     i = i + 2;
+
+ }
+
+
